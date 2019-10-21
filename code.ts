1b8436c7614a186ec2fb8228a1c3638e3eb26d5c
Bahadir <bahadrix@gmail.com>
2019-10-21 14:58:37 +0300

    Initial commit

---
+++ code.ts
@@ -0,0 +1,34 @@
+// This plugin creates 5 rectangles on the screen.
+const numberOfRectangles = 5
+
+let count = 0
+
+let firstSlice = null;
+let visiblityTarget = false;
+
+function traverse(node) {
+  console.log(node.type);
+
+  if (node.type == "SLICE") {
+    if (firstSlice === null) {
+      firstSlice = node;
+      visiblityTarget = !firstSlice.visible;
+    }
+    node.visible = visiblityTarget;
+  }
+
+  if ("children" in node) {
+    if (node.type !== "INSTANCE") {
+      for (const child of node.children) {
+        traverse(child)      
+      }
+    }
+  }
+}
+
+traverse(figma.root) // start the traversal at the root
+
+
+
+
+figma.closePlugin();
